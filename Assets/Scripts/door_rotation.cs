3ed14dde17d7db1e0a7a9aa0156c6541fe20a59e
Grigory .L <grigorylunev@gmail.com>
2021-04-24 09:35:32 +0300

    hw2_done

---
+++ Assets/Scripts/door_rotation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class door_rotation : MonoBehaviour
+{
+    private Animator anim;
+    private AudioSource Entry_door_sound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        Entry_door_sound = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        anim.SetBool("opening", true);
+        Entry_door_sound.Play();
+        Entry_door_sound.PlayDelayed(5f);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        anim.SetBool("opening", false);
+        Entry_door_sound.Play();
+        Entry_door_sound.PlayDelayed(5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/inside_door_2.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inside_door_2 : MonoBehaviour
+{
+    private Animator anim;
+    private AudioSource Entry_door_sound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        Entry_door_sound = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        anim.SetBool("opening", true);
+        Entry_door_sound.Play();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        anim.SetBool("opening", false);
+        Entry_door_sound.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
